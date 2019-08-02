7e7730f07a80e5c23d1d0263372f15e7644985ea
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-08-02 14:30:47 +0900

    pagelistを追加

---
+++ components/pageList.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+
+    <v-flex xs12>
+    <div v-for="i in postImage.length">
+      <v-img
+        :src="postImage[i-1].image_url"
+        class="my-3"
+        contain
+        height="200"
+      ></v-img>
+    </div>
+    </v-flex>
+
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      postImage: 'https://haniwaman.com/wp-content/uploads/2018/01/loading-840x600.png',
+      URL: "",
+    };
+  },
+  methods: {
+  },
+  async created() {
+    try {
+        // await axios({
+        //   method  : 'GET',
+        //   url     : "https://noben.herokuapp.com/notes",
+        //   headers: {
+        //   'Access-Control-Allow-Origin': '*',
+        //   'Content-Type': 'application/json',
+        //   },}
+        // )
+        await axios.get("https://noben.herokuapp.com/notes")
+        .then((res) => {
+          // alert(res.data[4].image_url);
+          this.postImage = res.data;
+          // axios.get(this.URL,{crossdomain: true})
+          // .then((res) => {
+          //   this.postImage = res.data;
+          // })
+          // .catch( (e) =>{
+          //   console.log(e);
+          // });
+        })
+        .catch( (e) =>{
+            console.log(e);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+  }
+};
+</script>
