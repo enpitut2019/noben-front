50fb00775a64e2e2fdd91a35ffc2bfb5f9f47414
keisuke-777 <keisuke7771120@me.com>
2019-08-02 14:11:36 +0900

    first commit Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <!-- <logo /> -->
+        <!-- <vuetify-logo /> -->
+      </div>
+      <div>
+        <getImage/>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import getImage from '~/components/getImage.vue'
+
+export default {
+  components: {
+    // Logo,
+    // VuetifyLogo,
+    getImage,
+  }
+}
+</script>
