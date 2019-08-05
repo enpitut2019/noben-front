a656d16c8d8c279d234f632be06e3d1db893bd41
keisuke-777 <keisuke7771120@me.com>
2019-08-05 12:22:28 +0900

    add routing for DetailPage

--- components/pageList.vue
+++ components/pageList.vue
@@ -7,12 +7,14 @@
 
     <v-flex xs12>
     <div class="note" v-for="i in postImage.length">
-      <v-img
-        :src="postImage[i-1].image_url"
-        class="my-3"
-        contain
-        height="200"
-      ></v-img>
+      <a v-on:click="loadDetail(id)">
+        <v-img
+          :src="postImage[i].image_url"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </a>
     </div>
     </v-flex>
 
@@ -28,31 +30,20 @@ export default {
     return {
       postImage: 'https://haniwaman.com/wp-content/uploads/2018/01/loading-840x600.png',
       URL: "",
+      id: 1,
     };
   },
   methods: {
+    loadDetail(id){
+      window.location.href = "https://noben.herokuapp.com/notes/" + id
+    }
   },
   async created() {
     try {
-        // await axios({
-        //   method  : 'GET',
-        //   url     : "https://noben.herokuapp.com/notes",
-        //   headers: {
-        //   'Access-Control-Allow-Origin': '*',
-        //   'Content-Type': 'application/json',
-        //   },}
-        // )
         await axios.get("https://noben.herokuapp.com/notes")
         .then((res) => {
           // alert(res.data[4].image_url);
           this.postImage = res.data;
-          // axios.get(this.URL,{crossdomain: true})
-          // .then((res) => {
-          //   this.postImage = res.data;
-          // })
-          // .catch( (e) =>{
-          //   console.log(e);
-          // });
         })
         .catch( (e) =>{
             console.log(e);
---
+++ pages/DetailPage/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <getImage/>
+    </div>
+</template>
+
+<script>
+import getImage from '~/components/getImage.vue'
+
+export default {
+  components: {
+    getImage,
+  }
+}
+</script>
