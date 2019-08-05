62f311e83a85dbdea18c4fe8e291c4c5ea69edfe
SUSUKE <cesium321yo@gmail.com>
2019-08-05 13:27:09 +0900

    コメントフォームを作成

---
+++ components/Comment.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div>
+            <!--h4>記事投稿</h4-->
+            <div>
+                <!--input v-model="title" type="text" name="title" placeholder="タイトル"-->
+                <textarea v-model="content" class="form-control" rows="4" placeholder=""></textarea>
+                <!--p>{{ title }}</p-->
+                <p>{{ content }}</p>
+                <button v-on:click="postComment">投稿</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        // メソッドの実行
+        // created() {
+        // },
+        // メソッドで使う&テンプレート内で使う変数を定義
+        data() {
+            return {
+                //title:'',
+                content:''
+            }
+        },
+        // (読み込み時に)実行するメソッド
+        methods: {
+            postComment(){
+                var comment = {
+                    //'title': this.title,
+                    'content': this.content
+                };
+                //var id = 1;
+                axios.post('http://httpbin.org/post', comment).then(res => {
+                    //console.log(res.data.title);
+                    console.log(res.data.content);
+                });
+            }
+
+        }
+    }
+</script>
