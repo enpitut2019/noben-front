f693dca207ed37602d6b9a510090bdb64f5c4762
keisuke-777 <keisuke7771120@me.com>
2019-08-05 12:46:51 +0900

    add vue router

--- pages/DetailPage/_id.vue
+++ pages/DetailPage/_id.vue
@@ -1,7 +1,10 @@
 <template>
+  <v-container>
     <div>
       <getImage/>
     </div>
+    {{text}}
+  </v-container>
 </template>
 
 <script>
@@ -10,6 +13,19 @@ import getImage from '~/components/getImage.vue'
 export default {
   components: {
     getImage,
+  },
+  data() {
+    return {
+      postImage: 'https://haniwaman.com/wp-content/uploads/2018/01/loading-840x600.png',
+      text: "text"
+    };
+  },
+  methods: {
+
+  },
+  created() {
+    alert($route.params.id)
+    // this.text = $route.params.id
   }
-}
+};
 </script>
