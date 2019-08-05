6c8223ce5c4d51761867300d3ad1776ef5171c96
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-08-05 09:35:18 +0900

    Upload.vue追加

---
+++ components/Upload.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+
+    <v-flex xs12>
+      <!-- <img :src="URL"> -->
+      <v-img
+        :src="postImage"
+        class="my-3"
+        contain
+        height="200"
+      ></v-img>
+    </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      postImage: 'https://haniwaman.com/wp-content/uploads/2018/01/loading-840x600.png',
+      URL: "",
+    };
+  },
+  methods: {
+  },
+  async created() {
+    try {
+        await axios.get("https://noben.herokuapp.com/notes")
+        .then((res) => {
+          // alert(res.data[4].image_url);
+          this.postImage = res.data[0].image_url;
+          // axios.get(this.URL,{crossdomain: true})
+          // .then((res) => {
+          //   this.postImage = res.data;
+          // })
+          // .catch( (e) =>{
+          //   console.log(e);
+          // });
+        })
+        .catch( (e) =>{
+            console.log(e);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+  }
+};
+</script>
