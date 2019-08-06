0c48b6704cae3f3263cba928fefea4efea554794
Wadami Kazuma <s1711581@s.tsukuba.ac.jp>
2019-08-06 15:08:14 +0900

    タグ機能追加

--- components/Upload.vue
+++ components/Upload.vue
@@ -3,14 +3,35 @@
   <v-layout text-center wrap>
 
     <div id="app">
-    <div class="fm">
-      <input type="file" @change="onFileChanged" multiple>
-      <button @click="onUpload">アップロード</button>
-      <div v-for="image in images">
+			<v-form ref = "form">
+				<v-text-field
+					v-model = "subjectName"
+					label = "教科名"
+					placeholder = "線形代数"
+					required
+					></v-text-field>
+        <v-icon>mdi-anchor</v-icon>
+				<v-text-field
+					v-model = "tags"
+					prepend-icon="close"
+					label = "タグ"
+					placeholder = "3回目, 課題"
+					required
+					></v-text-field>
+				<v-file-input
+				  label="ノート画像"
+			    @change = "onFileChanged"
+          multiple
+					></v-file-input>
+			</v-form>
+      <div>
+        <input type="file" @change="onFileChanged" multiple>
+        <button @click="onUpload">Upload</button>
+      </div>
+      <div v-for="(image,index) in images">
         <h2>{{image.name}}</h2>
         <img v-show="image" :src="image.thumbnail" />
       </div>
-    </div>
     </div>
 
 
@@ -25,48 +46,58 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      uploadedImages: [],
+      subjectName: '',
+      tags: '',
       images: []
     }
   },
   methods: {
     onFileChanged(event) {
-      this.uploadedImages = event.target.files
-      for(var i = 0;i < this.uploadedImages.length;i++) {
-        this.createImage(this.uploadedImages[i])
+      let files = event.target.files;
+      for(var i = 0;i < files.length;i++) {
+        this.createImage(files[i])
       }
     },
     onUpload() {
       const formData = new FormData()
-      formData.append('note[subject_name]', 'hoge')
+      formData.append('note[subject_name]', this.subjectName)
       for (  var i = 0;  i < this.images.length;  i++  ) {
-        formData.append(`note[pages_attributes][${i}][image]`, this.images[i].image, this.images[i].name)
+        formData.append(`note[pages_attributes][${i}][image]`, this.images[i].file, this.images[i].name)
         formData.append(`note[pages_attributes][${i}][order]`, 0)
       }
-      formData.append('tags', 'aaa, bbb, ccc')
+      formData.append('tags', this.tags)
       axios.post('https://noben.herokuapp.com/notes', formData)
     },
-    createImage(image) {
+    createImage(file) {
       const reader = new FileReader();
       let obj = {};
       reader.onload = e => {
         obj.thumbnail = e.target.result;
-        obj.image = image
-        obj.name = image.name;
+        obj.file = file;
+        obj.name = file.name;
         this.images.push(obj);
       };
-      reader.readAsDataURL(image);
+      reader.readAsDataURL(file);
     },
-    remove() {
-      this.sFile = false;
-    },
+    // remove(index) {
+    //   this.images[index] = null;
+    // },
   }
 
 };
 </script>
 
 <style>
-  .fm button {
+  .string-input {
+    color: black;
+    background-color: white;
+  }
+
+  .string-input::placeholder {
+    color: gray;
+  }
+
+  button {
         color: black;
         background-color: white;
         /*margin: auto;
@@ -77,7 +108,7 @@ export default {
         padding: 2px 8px;
     }
 
-    .fm input, button:focus {
+    input, button:focus {
         outline: none;
     }
 
