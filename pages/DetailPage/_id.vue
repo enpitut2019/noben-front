64607208d1838b1255dd66160e4f7f7562654002
keisuke-777 <keisuke7771120@me.com>
2019-08-07 09:44:18 +0900

    詳細ページの読み込みcssを追加

--- pages/DetailPage/_id.vue
+++ pages/DetailPage/_id.vue
@@ -9,45 +9,44 @@
     </div-->
 
     <!--h2>このノートについているタグ</h2-->
-    <hr>
-    <center>
-    タグ：
-    <div class="tag" v-for='tag in postImage.tags'>
-      <button v-on:click="searchUrl(tag.name)">{{tag.name}}</button>
-    </div></center><br><br>
-
-    <div class="pgft">
+    <div v-if="display">
+      <hr>
       <center>
-      <button class="btn" v-on:click="beforePage">前のページ</button>
-      <b v-if="maxPageNum != 0">{{nowPageNum + 1}} / {{maxPageNum}}</b>
-      <b v-else>{{nowPageNum}} / {{maxPageNum}}</b>
-      <button class="btn" v-on:click="nextPage">次のページ</button>
-      </center>
+      タグ：
+      <div class="tag" v-for='tag in postImage.tags'>
+        <button v-on:click="searchUrl(tag.name)">{{tag.name}}</button>
+      </div></center><br><br>
+    
+    
+      <div class="pgft">
+        <center>
+        <button class="btn" v-on:click="beforePage">前のページ</button>
+        <b v-if="maxPageNum != 0">{{nowPageNum + 1}} / {{maxPageNum}}</b>
+        <b v-else>{{nowPageNum}} / {{maxPageNum}}</b>
+        <button class="btn" v-on:click="nextPage">次のページ</button>
+        </center>
+      </div>
+
+      <div class="image">
+        <br>
+        <v-img 
+          :src='nowPage.image'
+        ></v-img>
+        <br>
+      </div>
+
+      <div class="pgft">
+        <center>
+        <button class="btn" v-on:click="beforePage">前のページ</button>
+        <b v-if="maxPageNum != 0">{{nowPageNum + 1}} / {{maxPageNum}}</b>
+        <b v-else>{{nowPageNum}} / {{maxPageNum}}</b>
+        <button class="btn" v-on:click="nextPage">次のページ</button>
+        </center>
+      </div>
     </div>
-
-    <div class="image">
-      <br>
-      <v-img 
-        :src='nowPage.image'
-      ></v-img>
-      <br>
-    </div>
-
-    <div class="pgft">
-      <center>
-      <button class="btn" v-on:click="beforePage">前のページ</button>
-      <b v-if="maxPageNum != 0">{{nowPageNum + 1}} / {{maxPageNum}}</b>
-      <b v-else>{{nowPageNum}} / {{maxPageNum}}</b>
-      <button class="btn" v-on:click="nextPage">次のページ</button>
-      </center>
-    </div>
-
-    <!--h2>このノートについているタグ</h2>
-    <hr>
-    <div class="tag" v-for='tag in postImage.tags'>
-      <button v-on:click="searchUrl(tag.name)">{{tag.name}}</button>
-    </div><br--><br>
-
+    <div v-else class="loader"></div>
+    
+    <br>
     <!-- コメントを一覧表示 -->
     <h2>コメント欄</h2>
     <hr>
@@ -70,6 +69,8 @@
     </div>
     <br>
     <br>
+    <br>
+    <br>
   </v-container>
 </template>
 
@@ -89,7 +90,7 @@ export default {
       nowPage: 'https://haniwaman.com/wp-content/uploads/2018/01/loading-840x600.png',
       nowPageNum: 0,
       maxPageNum: 0,
-      name: ""
+      name: "",
     };
   },
   methods: {
@@ -224,5 +225,69 @@ export default {
       width: 300px;
       margin-top: 5px;
       margin-bottom: 5px;
+    }
+
+    .loader {
+      font-size: 10px;
+      margin: 50px auto;
+      text-indent: -9999em;
+      width: 11em;
+      height: 11em;
+      border-radius: 50%;
+      background: #ffffff;
+      background: -moz-linear-gradient(left, #ffffff 10%, rgba(255,255,255, 0) 42%);
+      background: -webkit-linear-gradient(left, #ffffff 10%, rgba(255,255,255, 0) 42%);
+      background: -o-linear-gradient(left, #ffffff 10%, rgba(255,255,255, 0) 42%);
+      background: -ms-linear-gradient(left, #ffffff 10%, rgba(255,255,255, 0) 42%);
+      background: linear-gradient(to right, #ffffff 10%, rgba(255,255,255, 0) 42%);
+      position: relative;
+      -webkit-animation: load3 1.4s infinite linear;
+      animation: load3 1.4s infinite linear;
+      -webkit-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+      transform: translateZ(0);
+    }
+    .loader:before {
+      width: 50%;
+      height: 50%;
+      background: #ffffff;
+      border-radius: 100% 0 0 0;
+      position: absolute;
+      top: 0;
+      left: 0;
+      content: '';
+    }
+    .loader:after {
+      background: #303030;
+      width: 75%;
+      height: 75%;
+      border-radius: 50%;
+      content: '';
+      margin: auto;
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+    }
+    @-webkit-keyframes load3 {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
+    }
+    @keyframes load3 {
+      0% {
+        -webkit-transform: rotate(0deg);
+        transform: rotate(0deg);
+      }
+      100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+      }
     }
 </style>
